b3e89701e83e640428bf97eb7ea8b6d33af6375b
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-13 12:59:15 +0500

    dir

--- install.sh
+++ install.sh
@@ -3,17 +3,7 @@ set -e
 
 # Ask for sudo once, keep it alive
 sudo -v
-
-# Keep sudo alive for 2 hours (refresh every 60 seconds)
-# This loop will end automatically when the script finishes
-(
-    end=$((SECONDS + 7200)) # 7200 seconds = 2 hours
-    while [ $SECONDS -lt $end ]; do
-        sudo -n true
-        sleep 60
-    done
-) 2>/dev/null &
-
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Directory where this script (and presets) live
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -33,5 +23,9 @@ fi
 echo "Running dotfiles/install.sh with preset answers..."
 cd "$HOME/dotfiles"
 cat "$SCRIPT_DIR/preset_dotfile.sh" | bash install.sh
+
+mkdir -p ~/.cache/swww && echo -e "Lanczos3\n/home/ahmad/Pictures/wallpapers/wallpaper-1.jpg" > ~/.cache/swww/eDP-1
+mkdir -p ~/.cache/swww && echo -e "Lanczos3\n/home/ahmad/Pictures/wallpapers/wallpaper-1.jpg" > ~/.cache/swww/Virtual-1
+
 
 echo "✅ All done!"
