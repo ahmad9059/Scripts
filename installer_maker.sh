f446ac56f75d58768d023888025dfe2368cdd39f
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-13 15:46:58 +0500

    d

--- installer_maker.sh
+++ installer_maker.sh
@@ -5,10 +5,10 @@ cd "$HOME"
 echo "📂 Working directory: $PWD"
 
 if [ -f install.sh ]; then
-    echo "✅ install.sh found, running..."
+    echo "install.sh found, running..."
     ./install.sh
 else
-    echo "⬇️ Downloading install.sh..."
+    echo "Downloading install.sh..."
     curl -fsSL https://raw.githubusercontent.com/ahmad9059/Scripts/main/install.sh -o install.sh
     chmod +x install.sh
     ./install.sh
