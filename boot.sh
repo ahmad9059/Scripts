48bb196a8c6db922ea523998308e9d855a38bcde
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-14 16:12:02 +0500

    c

--- boot.sh
+++ boot.sh
@@ -68,6 +68,17 @@ chmod +x /tmp/replace_reads.sh
 bash /tmp/replace_reads.sh
 chmod +x install.sh
 bash install.sh
+echo "${OK} Arch-Hyprland script Installed!${RESET}"
+
+# ===========================
+# dotfile banner Banner
+# ===========================
+
+echo -e "\033[1;31m     _       _    __ _ _             ___           _        _ _           \033[0m"
+echo -e "\033[1;32m  __| | ___ | |_ / _(_) | ___  ___  |_ _|_ __  ___| |_ __ _| | | ___ _ __ \033[0m"
+echo -e "\033[1;33m / _\` |/ _ \| __| |_| | |/ _ \/ __|  | || '_ \/ __| __/ _\` | | |/ _ \ '__|\033[0m"
+echo -e "\033[1;34m| (_| | (_) | |_|  _| | |  __/\__ \  | || | | \__ \ || (_| | | |  __/ |   \033[0m"
+echo -e "\033[1;35m \__,_|\___/ \__|_| |_|_|\___||___/ |___|_| |_|___/\__\__,_|_|_|\___|_|   \033[0m"
 
 # ===========================
 # Clone dotfiles repo
@@ -76,7 +87,7 @@ if [ -d "$HOME/dotfiles" ]; then
   echo "${NOTE} Folder 'dotfiles' already exists in ~, using it...${RESET}"
 else
   echo "${NOTE} Cloning dotfiles repo into ~...${RESET}"
-  if git clone --depth=1 https://github.com/ahmad9059/dotfiles.git "$HOME/dotfiles"; then
+  if git clone --depth=1 https://github.com/ahmad9059/dotfiles.git "$HOME/dotfiles" >>"$LOG_FILE" 2>&1; then
     echo "${OK} Repo cloned successfully.${RESET}"
   else
     echo "${ERROR} Failed to clone dotfiles repo. Exiting.${RESET}"
@@ -89,7 +100,7 @@ fi
 # ===========================
 echo "${NOTE} Running dotfiles/install.sh with preset answers...${RESET}"
 cd "$HOME/dotfiles"
-chmod +x dotfile_installer
+chmod +x dotfile_installer.sh
 bash dotfile_installer.sh
 
 # ===========================
