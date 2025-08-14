dfccfea4e8eeb1173910dd8d0a880563c9c6dd89
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-14 22:40:10 +0500

    c

--- complete.sh
+++ complete.sh
@@ -67,11 +67,12 @@ fi
 echo "${NOTE} Running Arch-Hyprland/install.sh with preset answers...${RESET}"
 cd "$HOME/Arch-Hyprland"
 
-wget -q -O ~/Arch-Hyprland/install-scripts/zsh.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/zsh.sh
 sed -i '/^[[:space:]]*read HYP$/c\HYP="n"' ~/Arch-Hyprland/install.sh
-wget -q -O /tmp/replace_reads.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/replace_reads.sh
-chmod +x /tmp/replace_reads.sh
-bash /tmp/replace_reads.sh
+sed -i '345i \
+wget -q -O ~/Arch-Hyprland/install-scripts/zsh.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/zsh.sh\n\
+wget -q -O /tmp/replace_reads.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/replace_reads.sh\n\
+chmod +x /tmp/replace_reads.sh\n\
+bash /tmp/replace_reads.sh ' ~/Arch-Hyprland/install.sh
 chmod +x install.sh
 bash install.sh
 echo "${OK} Arch-Hyprland script Installed!${RESET}"
