1673d34eddaf570303377097ad3cf94f391003fd
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-14 22:22:14 +0500

    c

--- install-quick.sh
+++ install-quick.sh
@@ -66,6 +66,7 @@ fi
 # ===========================
 echo "${NOTE} Running Arch-Hyprland/install.sh with preset answers...${RESET}"
 cd "$HOME/Arch-Hyprland"
+sed -i '/^[[:space:]]*read HYP$/c\HYP="n"' ~/Arch-Hyprland/install.sh
 sed -i '345i \
 wget -q -O ~/Arch-Hyprland/install-scripts/zsh.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/zsh.sh\n\
 sed -i '\''/^[[:space:]]*read HYP$/c\HYP="n"'\'' ~/Arch-Hyprland/install.sh\n\
